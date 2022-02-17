d61c49e2c25dedb2837a208ef224d0c1ccf6ff01
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2022-02-17 19:05:53 +0530

    redux: actions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('From hirva')
+// this indicates the type of the action (string)
+const BUY_CAKE = 'BUY_CAKE';
+
+// now we define our action
+// an action is an object that has a type property
+// {
+//     type: BUY_CAKE,
+//     info: 'First redux action'
+// }
+
+// an action creator is a function that returns an action
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
