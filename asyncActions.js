71478744fdf78dd42f9d1d80c05c1ea6290dacb5
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2022-03-03 23:11:44 +0530

    fetching data from API endpoint install: axios and redux-thunk

--- asyncActions.js
+++ asyncActions.js
@@ -1,5 +1,8 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const applyMiddleware = redux.applyMiddleware
+const thunkMiddleware = require('redux-thunk').default
+const axios = require('axios')
 
 // we need to define 3 things: state, action and the reducer
 // the state is an object with 3 properties
@@ -55,5 +58,33 @@ const reducer = (state = initialState, action) => {
     }
 }
 
-// final step: create the redux store
-const store = createStore(reducer)
+// this is an action-creator
+// final step: to define the async action creator
+const fetchUsers = () => {
+    return function(dispatch) {
+        dispatch(fetchUsersRequest())
+        // her's an axios request to get the data from an API endpoint
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            // if the request is success, then we get back the response
+            // response.data is the array of users
+            // .map(user=>user.id) is used here to just to map through API and return all user's id instead of flooding our store with whole info from the API
+            const users = response.data.map(user => user.id)
+            dispatch(fetchUsersSuccess(users))
+        })
+        .catch(error => {
+            // if response failed, we get back an error
+            // error.message is the error description
+            dispatch(fetchUsersFailure(error.message))
+        })
+    }
+}
+
+// next: create the redux store
+// we will apply the redux-thunk middleware to our redux store
+// redux-thunk is used to Define async action creators by just putting the thunkMiddleware as an argument to the store's middleware
+const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+
+// finally at last we subscribe to our store and then dispatch the action creator
+store.subscribe(() => { console.log(store.getState()) })
+store.dispatch(fetchUsers())
